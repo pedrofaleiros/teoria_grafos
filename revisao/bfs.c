a723951360c273f3f098741c6b6138ef5a010789
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2022-09-29 15:40:06 +0000

    bfs e bfs dist

--- revisao/bfs.c
+++ revisao/bfs.c
@@ -3,6 +3,7 @@
 
 typedef struct vertice{
     int visitado;
+    int distancia;
     int lista_adj[100];
     int tamanho_lista;
 }vertice;
@@ -72,33 +73,70 @@ void mostra(fila * f)
     }
 }
 
-void bfs(vertices * v, int i)
+void bfs(vertice * v, int pos)
 {
     fila * f = malloc(sizeof(fila));
+    int cur, i;
 
-    
+    push(f, pos);
+
+    while(f->qtd > 0){
+        cur = pop(f);
+        if(v[cur].visitado == 0){
+            printf("%d ", cur);
+            v[cur].visitado = 1;
+            for(i = 0; i < v[cur].tamanho_lista; i++)
+                push(f, v[cur].lista_adj[i]);
+        }
+    }
+}
 
 
+void bfs_distancia(vertice * v, int pos)
+{
+    fila * f = malloc(sizeof(fila));
+    int cur, i, filho;
+
+    push(f, pos);
+
+    while(f->qtd > 0){
+        cur = pop(f);
+        v[cur].visitado = 1;
+
+        for(i = 0; i < v[cur].tamanho_lista; i++){
+            filho = v[cur].lista_adj[i];
+            if(v[filho].visitado == 0){
+                if(v[filho].distancia == 0 || v[filho].distancia > v[cur].distancia+1)
+                    v[filho].distancia = v[cur].distancia+1;
+                push(f, filho);
+            }
+        }
+    }
 }
 
 int main(){
 
-    int n, a, i, x, y;
+    int n, i, x, y;
     vertice * vertices;
 
-    scanf("%d %d", &n, &a);
+    scanf("%d", &n);
 
     vertices = malloc(sizeof(vertice)*(n+1));
 
-    for(i = 0; i < a; i++){
+    while(1){
         scanf("%d %d", &x, &y);
 
-        vertices[x].lista_adj[ vertices[x].tamanho_lista++ ] = y;
+        if(x == 0 || y == 0) break;
 
+        vertices[x].lista_adj[ vertices[x].tamanho_lista++ ] = y;
         vertices[y].lista_adj[ vertices[y].tamanho_lista++ ] = x;
     }
 
-    bfs(vertices, 1);
+
+    printf("\nbfs: ");
+    bfs_distancia(vertices, 1);
+
+    mostra_grafo(vertices, n);
 
     printf("\n");
     return 0;
@@ -107,9 +145,9 @@ int main(){
 void mostra_grafo(vertice * v, int n)
 {
     for(int i = 1; i <= n; i++){
-        printf("\n%d -> ", i);
-        for(int j = 0; j < v[i].tamanho_lista; j++){
+        printf("\n%d -> %d", i, v[i].distancia);
+        /* for(int j = 0; j < v[i].tamanho_lista; j++){
             printf("%d ", v[i].lista_adj[j]);
-        }
+        } */
     }
 }
