101015d688a13067a5bab3e61de5bd1415f2a955
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2022-11-05 20:15:31 -0300

    dijkstra

--- dijkstra/dijkstra.c
+++ dijkstra/dijkstra.c
@@ -1,5 +1,7 @@
 #include "min_heap.h"
 
+#define INF 9999
+
 typedef struct
 {
 	int visitado;
@@ -9,6 +11,9 @@ typedef struct
 	int tamanho_lista;
 } Vertice;
 
+void dijkstra(Vertice * v, int root, int * vet, int num_v);
+void preenche_dist(int * vet, int n);
+
 int main()
 {
 	int num_vertices;
@@ -16,12 +21,12 @@ int main()
 	scanf("%d", &num_vertices);
 
 	Vertice * vertices = malloc(sizeof(Vertice)*(num_vertices+1));
+	
 
 	int x, y, peso;
 	while(1)
 	{
 		scanf("%d %d %d", &x, &y, &peso);
-		printf("\n x: %d - y: %d - peso: %d\n", x, y, peso);
 
 		if(!x || !y || !peso) break;
 
@@ -32,13 +37,50 @@ int main()
 		vertices[y].lista_pesos[vertices[y].tamanho_lista++] = peso;
 	}
 
+	int * vetor = malloc(sizeof(int)*(num_vertices+1));
+
+	dijkstra(vertices, 1, vetor, num_vertices);
+
 	for(int i = 1; i <= num_vertices; i++){
-		printf("\nvertice %d -> ", i);
-		for(int j = 0; j < vertices[i].tamanho_lista; j++){
-			printf("(%d, p: %d) - ", vertices[i].lista_adj[j], vertices[i].lista_pesos[j]);
-		}
+		printf("\n distancia %d: %d", i, vetor[i]);
 	}
 
 	printf("\n");
 	return 0;
+}
+
+void dijkstra(Vertice * v, int root, int * vet, int num_v)
+{
+	Heap * heap = aloca_heap();
+	Dupla aux;
+	int cur, dist, i, elemento;
+
+	preenche_dist(vet, num_v);
+
+	vet[root] = 0;
+
+	push(heap, root, 0);
+
+	while(!empty(heap)){
+
+		aux = pop(heap);
+		cur = aux.current;
+		dist = aux.distancia;
+
+		for(i = 0; i < v[cur].tamanho_lista; i++){
+			elemento = v[cur].lista_adj[i];
+
+			if(dist + v[cur].lista_pesos[i] < vet[elemento]){
+				vet[elemento] = dist + v[cur].lista_pesos[i];
+				push(heap, elemento, vet[elemento]);
+			}
+		}
+	}
+}
+
+void preenche_dist(int * vet, int n)
+{
+	while(n > 0){
+		vet[n--] = INF;
+	}
 }
