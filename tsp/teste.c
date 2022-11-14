7abf6f1fb216a29f56c5caf3ad0f7bc6c230d0f3
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2022-11-13 23:04:56 -0300

    less mem

--- tsp/teste.c
+++ tsp/teste.c
@@ -35,23 +35,26 @@ float calcula_distancia_pontos(Ponto p1, Ponto p2)
 	return sqrt(distancia);
 }
 
-float distancia_ate_origin(Vertice * v, int dest, int origin);
-void calcula_distancia(Vertice * vertices, int origin, int * vis, int * rota, int indice, float distancia, int this_elemento);
-void calcula_rota(Vertice * vertices, int origin);
+float distancia_ate_origin(Vertice * v, int dest, int origin, int numv);
+void calcula_distancia(Vertice * vertices, int origin, int * vis, int * rota, int indice, float distancia, int this_elemento, int numv);
+void calcula_rota(Vertice * vertices, int origin, int numv);
+void tsp(Vertice * vs, int numv, int origin);
+void calcula(Vertice * vs, int numv, int elemento, int * vis, int * route, int indice);
 
 int * rota_otima;
 float menor_distancia = 9999;
-int numero_v = 0;
 
 int main()
 {
+	int numero_v = 0;
 	int i, j;
 	float x, y, distancia;
 	Ponto * pontos;
 	Ponto p1, p2;
-	Vertice * vertices = malloc(sizeof(Vertice)*numero_v);
 
 	scanf("%d", &numero_v);
+
+	Vertice * vertices = malloc(sizeof(Vertice)*numero_v);
 
 	pontos = malloc(sizeof(Ponto)*numero_v);
 	for(i = 0; i < numero_v; i++){
@@ -74,27 +77,67 @@ int main()
 	}
 
 	int origin = 0;
-
 	double t1 = omp_get_wtime();
-	calcula_rota(vertices, origin);
-
+	// calcula_rota(vertices, origin, numero_v);
+	tsp(vertices, numero_v, origin);
 	printf("Tempo: %lf\n", omp_get_wtime()-t1);
+	
+	/* 
 	printf("Quantidade de Lugares: %d\n", numero_v);
 	printf("Menor distancia: %f\n", menor_distancia);
 	printf("Rota: ");
 	for(i = 0; i <= numero_v; i++){
 		printf("%d ", rota_otima[i]+1);
-	}
+	} */
 
 	printf("\n");
 	return 0;
 }
 
-void calcula_rota(Vertice * vertices, int origin)
+void tsp(Vertice * vs, int numv, int origin){
+	int indice = 0;
+	int * route = malloc(sizeof(int)*(numv+1));
+	int * visitados = malloc(sizeof(int)*(numv));
+
+	route[0] = origin;
+	visitados[origin] = 1;
+
+	int i, elemento;
+
+	for(i = 0; i < vs[origin].tamanho_lista; i++){
+		elemento = vs[origin].lista_adj[i].valor;
+		calcula(vs, numv, elemento, visitados, route, indice+1);
+	}
+}
+
+void calcula(Vertice * vs, int numv, int elemento, int * vis, int * route, int indice){
+	route[indice] = elemento;
+	vis[elemento] = 1;
+
+	if(indice == numv-1){
+		int i;
+		/* printf("\nRoute: ");
+		for(i = 0; i <= numv; i++){
+			printf("%d ", route[i]);
+		} */
+	}else{
+		int i, el;
+		for(i = 0; i < vs[elemento].tamanho_lista; i++){
+			el = vs[elemento].lista_adj[i].valor;
+			if(vis[el] == 0){
+				calcula(vs, numv, el, vis, route, indice+1);
+			}
+		}
+	}
+
+	vis[elemento] = 0;
+}
+
+void calcula_rota(Vertice * vertices, int origin, int numv)
 {
-	int * visitados = malloc(sizeof(int)*numero_v);
-	int * rota = malloc(sizeof(int)*(numero_v+1));
-	rota_otima = malloc(sizeof(int)*(numero_v+1));
+	int * visitados = malloc(sizeof(int)*numv);
+	int * rota = malloc(sizeof(int)*(numv+1));
+	rota_otima = malloc(sizeof(int)*(numv+1));
 	int indice = 0, i, elemento;
 	float distancia = 0;
 
@@ -102,22 +145,22 @@ void calcula_rota(Vertice * vertices, int origin)
 
 	visitados[origin] = 1;
 	rota[0] = origin;
-	rota[numero_v] = origin;
+	rota[numv] = origin;
 
 	for(i = 0; i < tam_lista; i++){
 		elemento = vertices[origin].lista_adj[i].valor;
 		distancia = vertices[origin].lista_adj[i].peso;
-		calcula_distancia(vertices, origin, visitados, rota, indice+1, distancia, elemento);
+		calcula_distancia(vertices, origin, visitados, rota, indice+1, distancia, elemento, numv);
 	}
 
 	free(visitados);
 	free(rota);
 }
 
-void calcula_distancia(Vertice * vertices, int origin, int * vis, int * rota, int indice, float distancia, int this_elemento)
+void calcula_distancia(Vertice * vertices, int origin, int * vis, int * rota, int indice, float distancia, int this_elemento, int numv)
 {
 	if(distancia > menor_distancia) return;
-
+	
 	int i, tam_lista, el_da_lista;
 	float this_dist;
 
@@ -131,27 +174,28 @@ void calcula_distancia(Vertice * vertices, int origin, int * vis, int * rota, in
 		this_dist = vertices[this_elemento].lista_adj[i].peso;
 
 		if(vis[el_da_lista] == 0){
-			calcula_distancia(vertices, origin, vis, rota, indice+1, distancia+this_dist, el_da_lista);
+			calcula_distancia(vertices, origin, vis, rota, indice+1, distancia+this_dist, el_da_lista, numv);
 		}
 	}
 
-	if(indice == numero_v-1){
-		this_dist = distancia + distancia_ate_origin(vertices, this_elemento, origin);
+	if(indice == numv-1){
+		this_dist = distancia + distancia_ate_origin(vertices, this_elemento, origin, numv);
 
 		if(this_dist < menor_distancia){
 			menor_distancia = this_dist;
-			for(i = 0; i <= numero_v; i++){
+			for(i = 0; i <= numv; i++){
 				rota_otima[i] = rota[i];
 			}
 		}
 	}
+
 	vis[this_elemento] = 0;
 }
 
-float distancia_ate_origin(Vertice * v, int dest, int origin)
+float distancia_ate_origin(Vertice * v, int dest, int origin, int numv)
 {
 	int i;
-	for(i = 0; i < numero_v; i++){
+	for(i = 0; i < numv; i++){
 		if(v[dest].lista_adj[i].valor == origin){
 			return v[dest].lista_adj[i].peso;
 		}
--- tsp/tsp.c
+++ tsp/tsp.c
@@ -26,12 +26,12 @@ int main()
 		addAresta(vertices, y, x, peso);
 	}
 
-	for(i = 0; i < n; i ++){
+	/* for(i = 0; i < n; i ++){
 		printf("\n %d -> ", i);
 		for(j = 0; j < vertices[i].tamanho_lista; j++){
 			printf("(%d, peso: %d) ", vertices[i].lista_adj[j].valor, vertices[i].lista_adj[j].peso);
 		}
-	}
+	} */
 
 	int origin = 0;
 	calcula_rota(vertices, n, origin);
@@ -68,32 +68,25 @@ void calcula_rota(Vertice * vertices, int num_vertices, int origin)
 
 void calcula_distancia(Vertice * vertices, int num_v, int origin, int * vis, int * rota, int indice, int distancia, int this_elemento)
 {
+	if(distancia > menor_distancia) return;
+
 	int i, tam_lista, el_da_lista, this_dist;
 
-	int * copia_vis = malloc(sizeof(int)*num_v);
-	for(i = 0; i < num_v; i++){
-		copia_vis[i] = vis[i];
-	}
-
-	copia_vis[this_elemento] = 1;
+	vis[this_elemento] = 1;
 	rota[indice] = this_elemento;
 
 	tam_lista = vertices[this_elemento].tamanho_lista;
-	int flag = 1;
 
 	for(i = 0; i < tam_lista; i++){
 		el_da_lista = vertices[this_elemento].lista_adj[i].valor;
 		this_dist = vertices[this_elemento].lista_adj[i].peso;
 
-		if(copia_vis[el_da_lista] == 0){
-			flag = 0;
-			if(distancia+this_dist > menor_distancia) return;
-			calcula_distancia(vertices, num_v, origin, copia_vis, rota, indice+1, distancia+this_dist, el_da_lista);
+		if(vis[el_da_lista] == 0){
+			calcula_distancia(vertices, num_v, origin, vis, rota, indice+1, distancia+this_dist, el_da_lista);
 		}
 	}
 
-	// if(indice == num_v-1){
-	if(flag){
+	if(indice == num_v-1){
 		this_dist = distancia + distancia_ate_origin(vertices, num_v, this_elemento, origin);
 
 		if(this_dist < menor_distancia){
@@ -102,8 +95,8 @@ void calcula_distancia(Vertice * vertices, int num_v, int origin, int * vis, int
 				rota_otima[i] = rota[i];
 			}
 		}
-		return;
 	}
+	vis[this_elemento] = 0;
 }
 
 int distancia_ate_origin(Vertice * v, int num_v, int dest, int origin)
